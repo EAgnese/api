40eadcf2cbaf9d65dad552efc015e1604d2bc822
Agnese <neutron34980@gmail.com>
2022-03-29 00:32:29 +0200

    correction insert DB

--- bd/insertDB.sql
+++ bd/insertDB.sql
@@ -1,5 +1,5 @@
-INSERT INTO USERS(user_name, user_mail, user_password, user_point, user_access) VALUES ('admin','admin@admin.com','admin123',0,1)
-INSERT INTO USERS(user_name, user_mail, user_password, user_point, user_access) VALUES ('test','test@test.com','test123',0,0)
+INSERT INTO USERS(user_name, user_mail, user_password, user_points, user_access) VALUES ('admin','admin@admin.com','admin123',0,1);
+INSERT INTO USERS(user_name, user_mail, user_password, user_points, user_access) VALUES ('test','test@test.com','test123',0,0);
 
 INSERT INTO GAME_TYPES(game_type_libelle)  VALUES ('Souls-like');
 INSERT INTO GAME_TYPES(game_type_libelle)  VALUES ('Platformer');
@@ -10,20 +10,20 @@ INSERT INTO suggestions(suggestion_contents, suggestion_points, user_id)  VALUES
 INSERT INTO PLATFORMS(platform_libelle)  VALUES ('Xbox One');
 INSERT INTO PLATFORMS(platform_libelle)  VALUES ('PS4');
 
-INSERT INTO RUN_GATEGORIES(run_category_libelle)  VALUES ('Any%');
-INSERT INTO RUN_GATEGORIES(run_category_libelle)  VALUES ('100%');
+INSERT INTO RUN_CATEGORIES(run_category_libelle)  VALUES ('Any%');
+INSERT INTO RUN_CATEGORIES(run_category_libelle)  VALUES ('100%');
 
-INSERT INTO GAMES(game_libelle, game_image, game_type_code)  VALUES ('Demon Souls','Il est très vieux',1);
-INSERT INTO GAMES(game_libelle, game_image, game_type_code)  VALUES ('Elden Ring','Il est très beau',1);
+INSERT INTO GAMES(game_libelle, game_image, game_type_code)  VALUES ('Demon Souls','Il est très vieux',3);
+INSERT INTO GAMES(game_libelle, game_image, game_type_code)  VALUES ('Elden Ring','Il est très beau',3);
 INSERT INTO GAMES(game_libelle, game_image, game_type_code)  VALUES ('Super Mario Bros','C`est mario',1);
 
-INSERT INTO contains_run(run_category_code, game_code)  VALUES (1,1);
-INSERT INTO contains_run(run_category_code, game_code)  VALUES (1,2);
-INSERT INTO contains_run(run_category_code, game_code)  VALUES (1,3);
-INSERT INTO contains_run(run_category_code, game_code)  VALUES (2,1);
-INSERT INTO contains_run(run_category_code, game_code)  VALUES (2,2);
-INSERT INTO contains_run(run_category_code, game_code)  VALUES (2,3);
+INSERT INTO contains_run(run_category_code, game_code)  VALUES (1,10);
+INSERT INTO contains_run(run_category_code, game_code)  VALUES (1,11);
+INSERT INTO contains_run(run_category_code, game_code)  VALUES (1,12);
+INSERT INTO contains_run(run_category_code, game_code)  VALUES (2,10);
+INSERT INTO contains_run(run_category_code, game_code)  VALUES (2,11);
+INSERT INTO contains_run(run_category_code, game_code)  VALUES (2,12);
 
-INSERT INTO RUNS(run_time, game_code, run_category_code, platfrom_code, user_id)  VALUES ('1:02',2,1,1,1);
-INSERT INTO RUNS(run_time, game_code, run_category_code, platfrom_code, user_id)  VALUES ('1:09:51',2,1,1,1);
-INSERT INTO RUNS(run_time, game_code, run_category_code, platfrom_code, user_id)  VALUES ('2:04:51',3,1,1,1);
+INSERT INTO RUNS(run_time, game_code, run_category_code, platform_code, user_id)  VALUES ('1:02',10,1,1,1);
+INSERT INTO RUNS(run_time, game_code, run_category_code, platform_code, user_id)  VALUES ('1:09:51',10,1,1,2);
+INSERT INTO RUNS(run_time, game_code, run_category_code, platform_code, user_id)  VALUES ('2:04:51',12,1,1,2);
