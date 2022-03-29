3b7691f9a6e4f49fbd87e54f15bc8014a0819e80
Agnese <neutron34980@gmail.com>
2022-03-29 21:05:53 +0200

    pitié monsieur marche

--- bd/bd.js
+++ bd/bd.js
@@ -1,16 +1,15 @@
 const path = require("path");
 const { Pool } = require("pg");
 const fs = require("fs");
+require('dotenv').config()
 
+
+const isProduction = process.env.NODE_ENV === 'production'
 const pool = new Pool({
-    user: "iihhbweosayybd",
-    host: "ec2-52-212-228-71.eu-west-1.compute.amazonaws.com",
-    database: "d6q1jhkthpnfcl",
-    password: "b80edd6d73e7ec6fee056e79b98b887d9ce70269a08125beeb51bbe68d9b9590",
-    port: 5432,
-    max: 20,
-    ssl: true,
+    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    ssl: isProduction,
 });
+
 
 console.log("Connexion réussie à la base de données");
 
