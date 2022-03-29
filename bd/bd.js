887b1968961d3fb7ec735bff33bcb4472b111107
= <=>
2022-03-29 13:44:37 +0200

    test

--- bd/bd.js
+++ bd/bd.js
@@ -7,7 +7,9 @@ const pool = new Pool({
     host: "ec2-52-212-228-71.eu-west-1.compute.amazonaws.com",
     database: "d6q1jhkthpnfcl",
     password: "b80edd6d73e7ec6fee056e79b98b887d9ce70269a08125beeb51bbe68d9b9590",
-    port: 5432
+    port: 5432,
+    max: 20,
+    ssl: true,
 });
 
 console.log("Connexion réussie à la base de données");
