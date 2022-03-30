d478c02fff0045383e394a1ac1adabd3bc871f7c
= <=>
2022-03-30 14:59:48 +0200

    pitié marche

--- global.js
+++ global.js
@@ -1,5 +1,9 @@
 const {randomBytes} = require("crypto")
 const cookie_secret = "dashldhe128ewhgcvasdy7et2hvhwytt2"
-let SESSIONS = {}
+const SESSIONS = {}
 
-exports.default = SESSIONS,cookie_secret,randomBytes
+module.exports = {  
+    SESSIONS,
+    cookie_secret,
+    randomBytes
+} 
